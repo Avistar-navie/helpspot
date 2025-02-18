ad43f730874771f59e4f38b1e34f5036c69c3a15
Nataliia Vietchinkina <rediskins@gmail.com>
2025-02-18 17:40:59 +0100

    Added debugging statements

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 document.getElementById('request-form').addEventListener('submit', function (event) {
+    console.log('Form submitted');
     event.preventDefault(); // Prevent the form from submitting
 
     // Get form values
