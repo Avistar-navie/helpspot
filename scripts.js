af897acdbb7c4013835a6a406452fcd980086089
Nataliia Vietchinkina <rediskins@gmail.com>
2025-02-18 17:49:02 +0100

    Added debugging statements

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+console.log('scripts.js loaded');
 document.getElementById('request-form').addEventListener('submit', function (event) {
     console.log('Form submitted');
     event.preventDefault(); // Prevent the form from submitting
